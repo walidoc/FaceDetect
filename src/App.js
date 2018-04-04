5a7e82f78ebff55408a1d2a24a2f7fd637208ab6
walidoc <walidrahali10@gmail.com>
2018-04-04 23:57:41 +0100

    start using clarifai API to detect faces

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import Navigation from "./components/Navigation/Navigation";
-import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import Logo from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank";
 import Particles from 'react-particles-js';
 import './App.css';
 import 'tachyons';
+import Clarifai from 'clarifai';
+
+// instantiate clarifai with my API key
+const app = new Clarifai.App({
+    apiKey: 'a716d318ec1d4db9bcdc27b016434f08'
+});
 
 const particlesOptions = {
     particles: {
@@ -31,19 +38,31 @@ class App extends Component {
     constructor(){
         super();
         this.state = {
-            input : ''
+            input: '',
+            imageUrl: ''
         }
     }
 
     onInputChange = (event) => {
-         console.log(event.target.value);
          this.setState({
              input: event.target.value
          })
     }
 
     onButtonSubmit = () => {
-        console.log('clicked');
+        
+        this.setState({
+            imageUrl: this.state.input
+        })
+
+        app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+            function(response) {
+                console.log('response: ', response);
+            },
+            function(err) {
+            // there was an error
+            }
+        );
    }
 
     render() {
@@ -59,7 +78,7 @@ class App extends Component {
                     onInputChange={this.onInputChange} 
                     onButtonSubmit={this.onButtonSubmit} 
                 />
-                {/* <FaceRecognition /> */}
+                <FaceRecognition imageUrl={this.state.imageUrl} />
             </div>
         );
     }
